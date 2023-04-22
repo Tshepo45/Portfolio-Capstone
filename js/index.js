1d0afdc62fd290d5871e9867772b2b5decb58a7a
Tshep45 <tshepomakama45@gmail.com>
2023-04-22 18:10:29 +0200

    fix linters errors

--- js/index.js
+++ js/index.js
@@ -79,29 +79,24 @@ speaker.forEach((item) => {
 
 document.getElementById('speaker-container').innerHTML = speakers;
 
-// Set the date we're counting down to
-var countDownDate = new Date("nov 11, 2023 12:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
+let countDownDate = new Date("nov 11, 2023 12:00:00").getTime();
+
+let x = setInterval(function() {
+let now = new Date().getTime();
+let distance = countDownDate - now;
+let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+
   document.getElementById("timer").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
-  // If the count down is over, write some text 
+ 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "EXPIRED";
